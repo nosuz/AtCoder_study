029040ea7251421b59b9922ff8e838ef36e593a4
nosuz <nosuzuki@postcard.st>
2025-03-31 20:03:42 +0900

    Added code to read sample data on programs.

--- ABC_399/A.py
+++ ABC_399/A.py
@@ -1,20 +1,55 @@
-"""入力
+import io
+import os
+import sys
+
+try:
+    from dotenv import load_dotenv
+    load_dotenv()  # .env
+except ModuleNotFoundError:
+    pass  # ignore no dotenv error
+
+TEST_DATA = """
 6
 abcarc
 agcahc
+
+7
+atcoder
+contest
+
+8
+chokudai
+chokudai
+
+10
+vexknuampx
+vzxikuamlx
+
 """
 
-"""出力
-2
-"""
+if os.getenv("LOCAL_DEBUG"):
+    # trim \n
+    sys.stdin = io.StringIO(TEST_DATA[1:].replace('\n\n', '\n'))
 
-n = input()
-s = input()
-t = input()
 
-# sとtのペアを作成する。
-# x = [(a, b) for a, b in zip(s, t)]
-y = [True if a != b else False for a, b in zip(s, t)]
+def code():
+    n = input()
+    s = input()
+    t = input()
 
-# 合計で1の数を数え上げる。
-print(sum(y))
+    # sとtのペアを作成する。
+    # x = [(a, b) for a, b in zip(s, t)]
+    y = [True if a != b else False for a, b in zip(s, t)]
+
+    # 合計で1の数を数え上げる。
+    print(sum(y))
+
+
+if os.getenv("LOCAL_DEBUG"):
+    while True:
+        try:
+            code()
+        except EOFError:
+            break
+else:
+    code()
--- toolbox.md
+++ toolbox.md
@@ -1,3 +1,69 @@
+## サンプルデータの読み込み
+
+サンプルをコピペして標準入力から読み込ませるのはめんどくさい。
+
+- StringIO を使用する。
+- 環境変数を参照して、ローカルと提出環境を判断する。
+
+仮想環境を作成する。
+
+```bash
+sudo apt update
+sudo apt install python3.12-venv
+python3 -m venv ~/.at_coder
+. ~/.at_coder/bin/activate
+pip install dotenv
+```
+
+.env に環境変数を設定する。
+
+```
+LOCAL_DEBUG=T
+```
+
+プログラムに環境変数で標準入力を読むか・サンプルデータを読むか分岐するコードを入れる。
+
+```python
+import io
+import os
+import sys
+
+try:
+    from dotenv import load_dotenv
+    load_dotenv()  # .env
+except ModuleNotFoundError:
+    pass  # ignore no dotenv error
+
+TEST_DATA = """
+6
+abcarc
+agcahc
+
+7
+atcoder
+contest
+
+"""
+
+if os.getenv("LOCAL_DEBUG"):
+    # trim \n and blank lines
+    sys.stdin = io.StringIO(TEST_DATA[1:].replace('\n\n', '\n'))
+
+
+def code():
+    pass
+
+if os.getenv("LOCAL_DEBUG"):
+    while True:
+        try:
+            code()
+        except EOFError:
+            break
+else:
+    code()
+
+```
+
 ### 標準入力から読み込み
 
 ```python
