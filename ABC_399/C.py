a791eff9ffee8377937d9c82fd00ffe6c03bac8f
nosuz <norisuzu@gmail.com>
2025-04-04 18:15:56 +0900

    Completed ABC 399 C

--- ABC_399/C.py
+++ ABC_399/C.py
@@ -30,53 +30,63 @@
 4 6
 5 6
 
+4 6
+1 2
+2 3
+1 4
+1 3
+2 4
+3 4
+
+5 8
+1 2
+2 3
+1 3
+1 4
+3 4
+3 5
+4 5
+2 5
+
 """
-
-
-def follow(step, stem, leaf):
-    used_tree[leaf] = step
-    loop = 0
-
-    for next in tree[leaf]:
-        if next != stem:
-            if used_tree[next] == 0:
-                # not visited
-                if step < 1500:
-                    loop += follow(step + 1, leaf, next)
-            else:
-                # looping
-                if used_tree[next] < step:
-                    loop += 1
-                else:
-                    # already counted
-                    pass
-
-    return loop
-
 
 n, m = map(int, input().split())
 # n: number of nodes
 # m: number of path
 # [set()] * (n + 1) not work. all items share one Set.
-tree = [set() for _ in range(n + 1)]
-used_tree = [0 for _ in range(n + 1)]
+nodes = [set() for _ in range(n + 1)]
+visited = [False for _ in range(n + 1)]
 
 for _ in range(m):
     u, v = map(int, input().split())
-    if not v in tree[u]:
-        tree[u].add(v)
-        if not u in tree[v]:
-            tree[v].add(u)
+    if not v in nodes[u]:
+        nodes[u].add(v)
+        if not u in nodes[v]:
+            nodes[v].add(u)
 # print(tree)
 
 loop = 0
-step = 1
-for i in range(n + 1):
-    if used_tree[i] == 0:
-        # 0 is not visited
-        used_tree[i] = step
-        for next in tree[i]:
-            if (i != next) and (used_tree[next] == 0):
-                # print(i, next)
-                loop += follow(step + 1, i, next)
+for i in range(len(nodes)):
+    if len(nodes[i]) == 0:
+        continue
+
+    # follow paths and stack branches with cutting traces.
+    stack = [i]
+    while len(stack) > 0:
+        this_node = stack[-1]
+        visited[this_node] = True
+
+        if len(nodes[this_node]) == 0:
+            # reached the terminal
+            stack.pop()
+        else:
+            next_node = nodes[this_node].pop()
+
+            if visited[next_node]:
+                loop += 1
+
+            nodes[next_node].remove(this_node)
+            if len(nodes[next_node]) > 0:
+                stack.append(next_node)
+
 print(loop)
