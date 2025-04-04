1c6ff188caf8939f025062f045198fb8608a7a92
nosuz <nosuzuki@postcard.st>
2025-04-04 23:04:11 +0900

    Updated validate.py to accept expected answer in sample data.

--- validate.py
+++ validate.py
@@ -17,16 +17,32 @@ def extract_test_data(filename):
 def run_prog_with_data(prog_name, data):
     blocks = data.strip().split("\n\n")  # Split into blocks by empty lines
     for index, block in enumerate(blocks):
+        # skip comment outed sample
+        if block[0] == '#':
+            continue
+
+        try:
+            input_data, expected_answer = block.split("#")
+            expected_answer = expected_answer.lstrip()
+        except ValueError:
+            input_data = block
+            expected_answer = None
         print(f"Input {index}")
-        print(block)
+        print(input_data)
         process = subprocess.Popen(["python3", prog_name], stdin=subprocess.PIPE,
                                    stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-        stdout, stderr = process.communicate(input=block)
-        print("")
+        stdout, stderr = process.communicate(input=input_data)
+        stdout = stdout.strip()
         print(f"Output {index}")
         print(stdout, stderr)
         if process.returncode != 0:
             break
+        elif expected_answer:
+            if stdout == expected_answer:
+                print("OK")
+            else:
+                print(f"WA, expected: {expected_answer}")
+        print()
 
 
 if __name__ == "__main__":
