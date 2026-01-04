c7c10cb1e602824941f9be3e45ff3fb3e8ecc05b
Norio Suzuki <norisuzu@gmail.com>
2026-01-04 17:09:29 +0900

    Install Java environment.

--- .devcontainer/post-create-commands.sh
+++ .devcontainer/post-create-commands.sh
@@ -13,3 +13,9 @@ if [ ! -f "$MARKER_FILE" ]; then
     # 初期化完了を示すマーカーファイルを作成
     touch "$MARKER_FILE"
 fi
+
+# install gradle
+export SDKMAN_DIR="$HOME/.sdkman"
+curl -s "https://get.sdkman.io" | bash
+source "$SDKMAN_DIR/bin/sdkman-init.sh"
+sdk install gradle 8.7
